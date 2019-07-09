e427da73c7679d1f2c32a2f2e6f733e030b35c86
vbrdnk <burdenyuchok@gmail.com>
2019-07-09 19:58:24 +0300

    update commit

--- src/app/certificates/certificates-container.component.ts
+++ src/app/certificates/certificates-container.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup} from '@angular/forms';
-import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
-import {switchMap} from 'rxjs/operators';
 import {CertificatesService} from './certificates.service';
 
 @Component({
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://certifie.herokuapp.com/api/notes'
-};
+  // apiUrl: 'https://certifie.herokuapp.com/api/notes'
+  apiUrl: 'http://localhost:3000/certificates'
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+};
